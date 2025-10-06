763b09f3f24d99adcd9e31752d0518ddb633c9b7
ayapgitreps <ayapparaj022@gmail.com>
2025-10-06 14:18:57 +0530

    Update mlops_project_model.py

--- mlops_project_model.py
+++ mlops_project_model.py
@@ -26,16 +26,14 @@ try:
     df=pd.read_csv(url)
     print("Successfully loaded data from Github.")
 
-
 #Loading the kaggle shopping mall dataset
 #df=pd.read_csv("/home/vboxuser/Downloads/customer_shopping_data.csv")
-
-#Changing the format for invoice_date column
-#df['invoice_date']=pd.to_datetime(df['invoice_date'],format='%d/%m/%Y')
 
 except Exception as e:
     print(f"An error occurred: {e}")
 
+#Changing the format for invoice_date column
+#df['invoice_date']=pd.to_datetime(df['invoice_date'],format='%d/%m/%Y')
 
 DATA_PATH=df
 MODEL_DIR="models"
